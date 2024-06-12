99a58a6504ab8a705cb0fa813ad385ac2b62abf1
K1ska123 <ro4evalex@gmail.com>
2024-06-12 13:17:38 +0300

    httpsOptions

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import * as cookieParser from 'cookie-parser'
-import * as express from 'express'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as express from 'express';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+    const httpsOptions = {
+        key: fs.readFileSync('/etc/letsencrypt/live/evotask.ru/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/evotask.ru/fullchain.pem'),
+    };
 
-	app.setGlobalPrefix('api')
-	app.use('/static/uploads', express.static('uploads'));
-	app.use(cookieParser())
-	app.enableCors({
-		origin: ['http://localhost:3000', 'http://87.228.9.112:3000', 'https://evotask.ru', 'https://evotask.ru:3000'],
-		credentials: true,
-		exposedHeaders: 'set-cookie'
-	})
+    const app = await NestFactory.create(AppModule, { httpsOptions });
 
-	await app.listen(4201)
+    app.setGlobalPrefix('api');
+    app.use('/static/uploads', express.static('uploads'));
+    app.use(cookieParser());
+    app.enableCors({
+        origin: ['http://localhost:3000', 'http://87.228.9.112:3000', 'https://evotask.ru', 'https://evotask.ru:3000'],
+        credentials: true,
+        exposedHeaders: 'set-cookie',
+    });
+
+    await app.listen(4201);
 }
-bootstrap()
+bootstrap();
